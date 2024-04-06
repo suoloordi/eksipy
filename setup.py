1a948e165ed976aaa1a96ec169210337f97173f6
baris <toker@textkernel.nl>
2024-04-06 16:45:58 +0200

    update requests-html

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 
 setup(
     name='eksipy',
-    version='2.0.5',
+    version='2.0.6',
     description='Unofficial API for Eksi Sozluk.',
     long_description=long_description,
     author='Yusuf Usta',
